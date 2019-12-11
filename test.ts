6bbab5abc2d275b566b88a49b7e4c1cb2ea5cb6f
robertsausg <58374163+robertsausg@users.noreply.github.com>
2019-12-11 00:17:40 +0000

    update readme

--- test.ts
+++ test.ts
@@ -1,1 +1,19 @@
 // tests go here; this will not be compiled when this package is used as a library
+input.onButtonPressed(Button.B, function () {
+    start = 0
+    Binbot.moveBinbot(0, 0, 0)
+})
+input.onButtonPressed(Button.A, function () {
+    start = 1
+})
+let start = 0
+let front = 0
+radio.setGroup(187)
+basic.forever(function () {
+    if (start == 1) {
+        Binbot.moveBinbot(input.acceleration(Dimension.X), input.acceleration(Dimension.Y), 0)
+        front = Binbot.requestSensor(DistanceSensors.FRONT)
+        console.logValue("Front Sensor", front)
+        basic.pause(100)
+    }
+})
