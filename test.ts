2f5007a026dec1b5689425e52bbf31a1182a3442
robertsausg <58374163+robertsausg@users.noreply.github.com>
2020-01-08 16:17:35 +0100

    publish with updated test script

--- test.ts
+++ test.ts
@@ -1,4 +1,5 @@
-// tests go here; this will not be compiled when this package is used as a library
+// tests go here; this will not be compiled when this
+// package is used as a library
 input.onButtonPressed(Button.B, function () {
     start = 0
     Binbot.moveBinbot(0, 0, 0)
@@ -7,14 +8,28 @@ input.onButtonPressed(Button.A, function () {
     start = 1
 })
 let start = 0
-let front = 0
+let sensor = 0
 radio.setGroup(187)
 basic.forever(function () {
+
     if (start == 1) {
         Binbot.moveBinbot(input.acceleration(Dimension.X), input.acceleration(Dimension.Y), 0)
-        front = Binbot.requestSensor(0)
+    } else {
+        Binbot.moveBinbot(0, 0, 0)
 
-        console.logValue("Front Sensor", front)
-        basic.pause(100)
     }
+    basic.pause(100)
+
+})
+basic.forever(function () {
+    for (let index = 0; index <= 8; index++) {
+        sensor = Binbot.requestSensor(index)
+        console.logValue("Sensor Value " + index.toString() + " : ", sensor)
+        if (sensor > 1000) {
+            start = 0
+            Binbot.moveBinbot(0, 0, 0)
+        }
+
+    }
+    basic.pause(100)
 })
