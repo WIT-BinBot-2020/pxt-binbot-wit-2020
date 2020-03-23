748e24f5e5a3212f697f29268881a274d03a592c
Jon Gillespie <jontygillespie@outlook.com>
2020-03-23 14:11:49 +0000

    Testing

--- pi_hub_python/ears/ears_test.py
+++ pi_hub_python/ears/ears_test.py
@@ -2,20 +2,38 @@
 import ears
 import time
 
-print("Starting EARS Thread")
+print("TEST >> Starting EARS Thread")
 ears.start_doa_thread()
 
-print("Sleeping...")
+print("TEST >> Sleeping...")
 time.sleep(5)
 
-print("Set new VAD Threshold")
-ears.set_vad_threshold(255)
-
-print("Sleeping...")
+print("TEST >> Stop Thread")
 time.sleep(5)
 
-print("Set new VAD Threshold")
-ears.set_vad_threshold(0)
 
-print("Sleeping...")
-time.sleep(5)
+while True:
+    time.sleep(2)
+    print("TEST >> loop...")
+
+
+
+
+
+
+
+
+print("TEST >> ")
+
+
+# print("TEST >> Set new VAD Threshold")
+# ears.set_vad_threshold(255)
+
+# print("TEST >> Sleeping...")
+# time.sleep(5)
+
+# print("TEST >> Set new VAD Threshold")
+# ears.set_vad_threshold(0)
+
+# print("TEST >> Sleeping...")
+# time.sleep(5)
